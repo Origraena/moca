1f16cad899b7c2da6ae19eeacd3eb20c22673558
DUVILLIE Guillerme <guillerme.duvillie@gmail.com>
2012-05-13 23:27:50 +0200

    Ca marche! (Enfin sur le cas choisi)

--- gmin213/tp/pratique/branch_and_bound_dev/tsp.c
+++ gmin213/tp/pratique/branch_and_bound_dev/tsp.c
@@ -50,6 +50,7 @@ int findACPM (tsp_t *t) {
 	int curnode = 0, nb_marked = 1, i;
 	int next[2] = {-1};
 	int mark_sol = 0;
+	int rate = 0;
 
 	for (i=0; i<t->nb_node; i++)
 		if (!nbvoisin(t, i)) {
@@ -68,40 +69,38 @@ int findACPM (tsp_t *t) {
 		t->sol[i] = -1;
 	}
 
-	while (nb_marked < t->nb_node && curnode != -1) {
+	while (nb_marked < t->nb_node && !rate) {
+		visited[curnode] ++;
+		nb_marked++;
+		// Update Cost of edges
+		for (i=0; i<t->nb_node; i++) 
+			if (!visited[i]) 
+				if (t->mat[curnode][i] != -1 && (t->mat[curnode][i] < mark[i] || mark[i] == -1)) {
+					mark[i] = t->mat[curnode][i];
+					src[i] = curnode;
+				}
+		
 		next[0] = -1;
 		next[1] = -1;
-		visited[curnode] ++;
-
-		for (i=0; i<t->nb_node; i++){
-			if (!visited[i]) {
-				if (t->mat[curnode][i] != -1) {
-					if (mark[i] == -1 || t->mat[curnode][i] < mark[i]) {
-						src[i] = curnode;
-						mark[i] = t->mat[curnode][i];
+		for (i=0; i<t->nb_node; i++) 
+			if (!visited[i]) 
+				if (mark[i] != -1) 
+					if (next[0] == -1 || mark[i] < next[0]) {
+						next[0] = mark[i];
+						next[1] = i;
 					}
-				}
-				if (next[0] == -1 || mark[i] < next[0]){
-					next[0] = mark[i];
-					next[1] = i;
-				}
-			}
-		}
 
-		if (next[1] != -1)
-			t->sol[mark_sol++] = src[next[1]] * t->nb_node + next[1];
+		if (next[1] == -1)
+			rate++;
+
+		t->sol[mark_sol++] = src[next[1]] * t->nb_node + next[1];
 		curnode = next[1];
-		nb_marked++;
 	}
 	free(visited);
 	free(src);
 	free(mark);
 
-	for (i=0; i<t->nb_node-2; i++)
-		if (t->sol[i] == -1)
-			return 0;
-
-	return nb_marked == t->nb_node ? 1 : 0;
+	return rate ? 0 : 1;
 }
 
 void initTSPFromFile (tsp_t **tsp, FILE *in) {
